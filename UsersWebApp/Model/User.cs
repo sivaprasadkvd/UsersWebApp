ebf88554d2728a02153b0694ccfe6548c7a15eb2
vijayadurgasivaprasa <vijayadurgasivaprasa@SNAD-74>
2023-12-19 15:32:41 +0530

    Add project files.

---
+++ UsersWebApp/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UsersWebApp.Model
+{
+    public class User
+    {
+
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
